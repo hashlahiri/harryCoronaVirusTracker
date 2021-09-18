2e9a5db32b31c9b54d2ca4a49f450f8d7b92842e
C Lahiri <helloclahiri@outlook.com>
2021-09-18 21:20:14 +0530

    Updated Version
    The updated version deployed

---
+++ corona-track/src/test/java/com/coronavirus/coronatrack/CoronaTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coronavirus.coronatrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
